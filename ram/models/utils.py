d60e5a4028829e214f5f6f40712ac3fe2c882704
liguanlin <517445163@qq.com>
2024-03-18 10:40:14 +0800

    remove path hard code

--- ram/models/utils.py
+++ ram/models/utils.py
@@ -128,7 +128,7 @@ class GroupWiseLinear(nn.Module):
 
 
 def init_tokenizer():
-    tokenizer = BertTokenizer.from_pretrained('/data/liguanlin/codes/research_project/Semi-UIR/ram/models/bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('./ram/models/bert-base-uncased')
     tokenizer.add_special_tokens({'bos_token': '[DEC]'})
     tokenizer.add_special_tokens({'additional_special_tokens': ['[ENC]']})
     tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]
